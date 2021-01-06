143546eb9c3902e5aba0d3ad643653829fcd7cd8
Eric Huber <echuber2@illinois.edu>
2021-01-06 04:42:01 -0600

    example

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # inline-float-literals
-A trick to get C++ floats to compile as inline integer literals
+A trick to get C++ floats to compile as inline integer literals.
+
+Someone asked, is there an easy way to get a float literal stored in the function call's local stack rather than in the program data section, as with an integer? This was the simplest way I could think to do it (in theory). It seems to work with Clang and GCC if you use `-O1` or `-O2`. I don't recommend actually using it for anything.
+
+For comparison, here's another approach to the idea: http://brnz.org/hbr/?p=1518
---
+++ inline-float-literals.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// https://github.com/echuber2/inline-float-literals
+// Compile with -O1 or -O2. This may not be portable (or standards-compliant).
+
+#define LIT_FLOAT(name, X, allow_flag) \
+do { \
+    if (allow_flag>0) { \
+        constexpr float mynum = (X); \
+        uint32_t tmp_int = *(uint32_t*)(&mynum) * allow_flag; \
+        *(float*)(&name) = *(float*)(&tmp_int); \
+    } \
+    else { \
+        constexpr float mynum = (X); \
+        uint32_t tmp_int = *(uint32_t*)(&mynum) * allow_flag; \
+        *(float*)(&name) = *(float*)(&tmp_int); \
+    } \
+} while(0);
+
+__attribute__((weak)) int allow() {
+    return 1;
+}
+
+int main() {
+
+    float f;
+    LIT_FLOAT(f, -2.5, allow());
+    std::cout << f << std::endl;
+
+    return 0;
+}
