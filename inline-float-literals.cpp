96929334998b35151b242636af2cecd0dd7ce7f5
Eric Huber <echuber2@illinois.edu>
2021-01-06 05:07:31 -0600

    another simplification

--- inline-float-literals.cpp
+++ inline-float-literals.cpp
@@ -3,21 +3,21 @@
 // https://github.com/echuber2/inline-float-literals
 // Compile with -O1 or -O2. This may not be portable (or standards-compliant).
 
-#define LIT_FLOAT(name, X, allow_flag) \
-do { \
-    constexpr float mynum = (X); \
-    uint32_t tmp_int = *(uint32_t*)(&mynum) * allow_flag; \
-    *(float*)(&name) = *(float*)(&tmp_int); \
-} while(0);
-
 __attribute__((weak)) int allow() {
     return 1;
 }
 
+#define LIT_FLOAT(name, X) \
+do { \
+    constexpr float mynum = (X); \
+    uint32_t tmp_int = *(uint32_t*)(&mynum) * allow(); \
+    *(float*)(&name) = *(float*)(&tmp_int); \
+} while(0);
+
 int main() {
 
     float f;
-    LIT_FLOAT(f, 2.5, allow());
+    LIT_FLOAT(f, 2.5);
     std::cout << f << std::endl;
 
     return 0;
